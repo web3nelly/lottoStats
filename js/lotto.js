817b26f95a5f809d5c239c4d3407410cbeac9c61
Bradford Nelson <b3nelly@hotmail.com>
2023-03-24 22:08:34 -0500

    added update function to umOfHotDrawings and umOfJackpots inputs

--- js/lotto.js
+++ js/lotto.js
@@ -1,26 +1,53 @@
-import megaMillionsDrawings from './mm.2019-5-15.js';
-import powerBallDrawings from './pb.2019-5-15.js';
-import LottoStatsDisplay from './lottoStatsDisplay.js';
+import megaMillionsDrawings from "./mm.2019-5-15.js";
+import powerBallDrawings from "./pb.2019-5-15.js";
+import LottoStatsDisplay from "./lottoStatsDisplay.js";
 
-let numOfHotDrawings = parseFloat(document.getElementById('numOfHotDrawings').value);
-let numOfJackpots = parseFloat(document.getElementById('numOfJackpots').value);
+const numOfHotDrawingsInput = document.getElementById("numOfHotDrawings");
+const numOfJackpotsInput = document.getElementById("numOfJackpots");
 
-const megaMillionsStats = new LottoStatsDisplay(megaMillionsDrawings, 70, 25, numOfHotDrawings);
-const megaMillionsDivId = 'megaMillions';
-const megaBall = 'Mega Ball';
+const megaMillionsDivId = "megaMillions";
+const megaBall = "Mega Ball";
+const powerBallDivId = "powerBall";
+const powerBall = "Power Ball";
 
-megaMillionsStats.displayNumbersDrawn(megaMillionsDivId);
-megaMillionsStats.displaySpecialNumbersDrawn(megaMillionsDivId, megaBall);
-megaMillionsStats.displayHotNumbers(megaMillionsDivId, 20);
-megaMillionsStats.displayHotSpecialNumbers(megaMillionsDivId, megaBall, 10);
-megaMillionsStats.displayTopJacks(megaMillionsDivId, numOfJackpots);
+function updateLottoStats() {
+  // Clear the contents of the div elements
+  document.getElementById(megaMillionsDivId).innerHTML = "";
+  document.getElementById(powerBallDivId).innerHTML = "";
 
-const powerBallStats = new LottoStatsDisplay(powerBallDrawings, 69, 26, numOfHotDrawings);
-const powerBallDivId = 'powerBall';
-const powerBall = 'Power Ball';
+  let numOfHotDrawings = parseFloat(numOfHotDrawingsInput.value);
+  let numOfJackpots = parseFloat(numOfJackpotsInput.value);
 
-powerBallStats.displayNumbersDrawn(powerBallDivId);
-powerBallStats.displaySpecialNumbersDrawn(powerBallDivId, powerBall);
-powerBallStats.displayHotNumbers(powerBallDivId, 20);
-powerBallStats.displayHotSpecialNumbers(powerBallDivId, powerBall, 10);
-powerBallStats.displayTopJacks(powerBallDivId, numOfJackpots);
+  const megaMillionsStats = new LottoStatsDisplay(
+    megaMillionsDrawings,
+    70,
+    25,
+    numOfHotDrawings
+  );
+
+  megaMillionsStats.displayNumbersDrawn(megaMillionsDivId);
+  megaMillionsStats.displaySpecialNumbersDrawn(megaMillionsDivId, megaBall);
+  megaMillionsStats.displayHotNumbers(megaMillionsDivId, 20);
+  megaMillionsStats.displayHotSpecialNumbers(megaMillionsDivId, megaBall, 10);
+  megaMillionsStats.displayTopJacks(megaMillionsDivId, numOfJackpots);
+
+  const powerBallStats = new LottoStatsDisplay(
+    powerBallDrawings,
+    69,
+    26,
+    numOfHotDrawings
+  );
+
+  powerBallStats.displayNumbersDrawn(powerBallDivId);
+  powerBallStats.displaySpecialNumbersDrawn(powerBallDivId, powerBall);
+  powerBallStats.displayHotNumbers(powerBallDivId, 20);
+  powerBallStats.displayHotSpecialNumbers(powerBallDivId, powerBall, 10);
+  powerBallStats.displayTopJacks(powerBallDivId, numOfJackpots);
+}
+
+// Add event listeners to the input elements
+numOfHotDrawingsInput.addEventListener("change", updateLottoStats);
+numOfJackpotsInput.addEventListener("change", updateLottoStats);
+
+// Initial display of the stats
+updateLottoStats();
